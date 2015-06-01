fc589f274e879a76753c23ca561ca32b727e3341
ignacioelola <ignacioelolavillar@gmail.com>
2015-06-01 12:55:22 +0100

    improvements: - handeling keys as arg - filter from argv - new sources for spain newspapers

--- filter.py
+++ filter.py
@@ -1,11 +1,12 @@
 __author__ = 'ignacioelola'
 
 import csv
+import sys
 
 with open("data/output.csv", "r") as infile:
     with open("data/filtered-output.csv", "w") as outfile:
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
         for row in reader:
-            if "cameron" in row[2].lower():
+            if str(sys.argv[1]) in row[2].lower():
                 writer.writerow(row)
--- get_data.py
+++ get_data.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# Description: Script to get MPs financial interests
 __author__ = 'ignacioelola'
 
 import csv
 import requests
 import json
 import time
+import sys
 import importio_rsc
 
 
@@ -13,20 +13,24 @@ def get_web_data(guid, url):
 
     query = {"input": {"webpage/url": url}}
     response = importio_rsc.query_api(query, guid)
-    data = response["results"][0].get("blob")
+    data = response["results"][0].get("text")
 
     return data
 
 
 def data_analyzer(text, source):
     data = {
-        'text_list': [text]
+        'text_list': [text],
+        'max_keywords': 25,
+        'use_company_names': 1,
+        'expand_acronyms': 1
+
     }
 
     response = requests.post(
-        "https://api.monkeylearn.com/v2/extractors/ex_y7BPYzNG/extract/",
+        "https://api.monkeylearn.com/v2/extractors/ex_eV2dppYE/extract/",
         data=json.dumps(data),
-        headers={'Authorization': 'Token YOUR-TOKEN',
+        headers={'Authorization': 'Token ' + str(sys.argv[1]),
                 'Content-Type': 'application/json'})
 
     results = json.loads(response.text)["result"][0]
@@ -46,19 +50,19 @@ def data_savior(data, filename):
         for record in data:
             row = []
             if "source" in record:
-                row.append(record["source"])
+                row.append(record["source"].encode("utf-8"))
             else:
                 row.append("")
             if "datetime" in record:
-                row.append(record["datetime"])
+                row.append(record["datetime"].encode("utf-8"))
             else:
                 row.append("")
             if "keyword" in record:
-                row.append(record["keyword"])
+                row.append(record["keyword"].encode("utf-8"))
             else:
                 row.append("")
             if "relevance" in record:
-                row.append(record["relevance"])
+                row.append(record["relevance"].encode("utf-8"))
             else:
                 row.append("")
             writer.writerow(row)
@@ -67,13 +71,19 @@ def data_savior(data, filename):
 
 if __name__ == '__main__':
 
-    sources = {"The Guardian UK Politics": {"guid": "7787398d-af1a-438f-9d0d-06521069c2cf",
-                                "source_url": "http://www.theguardian.com/politics"},
-               "The Telegraph UK Politics": {"guid": "3338f0f3-21c7-4b40-809b-a37440dc8e12",
-                                "source_url": "http://www.telegraph.co.uk/news/politics/"},
+    sources = {"El Pais": {"guid": "2d62f5a4-18d3-4fba-bac7-ba75e084b392",
+                                "source_url": "http://politica.elpais.com/"},
+                "El Mundo": {"guid": "39dfd0c6-fe9a-43d8-8eeb-146f7007a873",
+                                "source_url": "http://www.elmundo.es/espana.html?cid=MENUHOM24801"},
+                "ABC": {"guid": "117d0acd-b808-45c9-8862-904321f90145",
+                                "source_url": "http://m.abc.es/espana.html"},
+                "La Razon": {"guid": "04a9c37a-1520-41ea-a664-9972648035fe",
+                                "source_url": "http://www.larazon.es/espana"},
+                "El Diario": {"guid": "d651cf34-7de7-4728-9db7-678e234a472a",
+                                "source_url": "http://www.eldiario.es/politica/"},
                }
 
-    filename_output = "data/output.csv"
+    filename_output = "data/output_spain.csv"
 
     # Extract data
     for source in sources:
